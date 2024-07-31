8475e2baa2913908ada784ce61e05ec4c423d5ec
Gleb1371 <163561026+Gleb1371@users.noreply.github.com>
2024-07-31 18:40:34 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,22 @@
+# conftest.py
 
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Choose language: es, ru, fr, etc.")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_argument(f"--lang={language}")
+    
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service, options=options)
+    
+    yield browser
+    browser.quit()
