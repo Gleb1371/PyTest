fcfa36e0ab17f045e4fa217f6e34e84257df1955
Gleb1371 <163561026+Gleb1371@users.noreply.github.com>
2024-07-31 18:41:14 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,8 @@
-
+def test_guest_should_see_add_to_basket_button(browser):
+    # URL продукта для теста
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    
+    # Ищем кнопку добавления в корзину
+    add_to_basket_button = browser.find_element("css selector", ".btn-add-to-basket")
+    assert add_to_basket_button is not None, "Add to basket button not found on the page"
